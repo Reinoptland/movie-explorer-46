a0e7bff630c85d04279e7196ca5ace280f0898a3
Reinoptland <reinoptland@gmail.com>
2020-10-15 14:52:32 +0200

    Install dependencies

---
+++ plan.md
@@ -0,0 +1,29 @@
+## Routing
+
+- Routing, y tho? (and what is it)
+
+  - Express
+
+    - app.get('/', (req, res) => res.send())
+    - app.get('/jokes', (req, res) => res.send('joke'))
+    - Send HTML files
+
+  - React
+    - SPA
+    - We don't want to refresh
+    - Cannot use `<a>` tags
+
+- React Router
+- Setup
+
+  - [] npm install react-router-dom
+  - [] wrap our app in a Router component (index.js)
+
+- Static Routes
+- Dynamic Routes
+
+## What we will build
+
+- App with different pages
+- Page with a list of movies
+- Page with details for a specific movie
--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [page, setPage] = useState("home");
+
+  console.log(page);
+
+  const content = page === "home" ? "Welcome" : "About";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setPage("home")}>Home</button>
+        <button onClick={() => setPage("about")}>About</button>
+
+        {content}
       </header>
     </div>
   );
